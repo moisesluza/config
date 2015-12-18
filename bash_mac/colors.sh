b0ded14c2ccc8db4acb0ad47ca0004e09abac140
Moises Luza <mluza@ucsd.edu>
2015-12-18 10:32:15 -0800

    adding configuration files for bash on mac

---
+++ bash_mac/colors.sh
@@ -0,0 +1,42 @@
+# Regular Colors
+BLACK='\033[0;30m' # Black
+RED='\033[0;31m' # Red
+GREEN='\033[0;32m' # Green
+YELLOW='\033[0;33m' # Yellow
+BLUE='\033[0;34m' # Blue
+PURPLE='\033[0;35m' # Purple
+CYAN='\033[0;36m' # Cyan
+WHITE='\033[0;37m' # White
+
+# High Intensty
+HI_BLACK='\033[0;90m' # Black
+HI_RED='\033[0;91m' # Red
+HI_GREEN='\033[0;92m' # Green
+HI_YELLOW='\033[0;93m' # Yellow
+HI_BLUE='\033[0;94m' # Blue
+HI_PURPLE='\033[0;95m' # Purple
+HI_CYAN='\033[0;96m' # Cyan
+HI_WHITE='\033[0;97m' # White
+
+# Background
+BG_BLACK='\033[40m' # Black
+BG_RED='\033[41m' # Red
+BG_GREEN='\033[42m' # Green
+BG_YELLOW='\033[43m' # Yellow
+BG_BLUE='\033[44m' # Blue
+BG_PURPLE='\033[45m' # Purple
+BG_CYAN='\033[46m' # Cyan
+BG_WHITE='\033[47m' # White
+
+# High Intensty backgrounds
+HI_BG_BLACK='\033[0;100m' # Black
+HI_BG_RED='\033[0;101m' # Red
+HI_BG_GREEN='\033[0;102m' # Green
+HI_BG_YELLOW='\033[0;103m' # Yellow
+HI_BG_BLUE='\033[0;104m' # Blue
+HI_BG_PURPLE='\033[10;95m' # Purple
+HI_BG_CYAN='\033[0;106m' # Cyan
+HI_BG_WHITE='\033[0;107m' # White
+
+#Replace any leading leading 0; with 1; for bold colors
+#Replace any leading 0; with 4; to underline
